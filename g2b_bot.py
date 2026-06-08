3bf9ed5c1b9747b4a939dd32aeb172d851f671c6
USER <tmdduqflfl@naver.com>
2026-06-08 17:08:42 +0900

    Feat: run.yml + g2b_bot v.0.03

--- g2b_bot.py
+++ g2b_bot.py
@@ -18,28 +18,27 @@ KEYWORDS = ['스쿨넷', '융합통신망', '교육망', '스마트기기']
 
 
 def get_jodal_pblanc():
-    """최근 6개월치(180일) 공고를 수집해서 오늘 기준 최신 업데이트/진행중 분류하기"""
+    """최근 6개월치 '발주계획'과 '입찰공고'를 모두 수집해서 분류하기"""
     now = datetime.datetime.now()
     today_str = now.strftime('%Y%m%d')
-
-    # 🌟 조회 기간을 30일에서 180일(6개월)로 대폭 확장!
     six_months_ago = (now - datetime.timedelta(days=180)).strftime('%Y%m%d')
 
-    new_items = []  # 오늘 새로 등록된 공고 [최신 업데이트]
-    ongoing_items = []  # 최근 6개월간 등록되어 진행 중인 공고
+    new_items = []  # 오늘 새로 등록된 건 [최신 업데이트]
+    ongoing_items = []  # 진행 중인 건
 
-    # 조달청 용역(Servc) 및 물품(Thng) 공고 API 주소
+    # 🌟 사장님 화면에 맞춰 '발주계획(OrderPlan)' API 주소를 최상단에 전격 추가!
     urls = [
-        "http://apis.data.go.kr/1230000/BidPublicInfoService04/getBidPblancListInfoServcPPSSrch",
-        "http://apis.data.go.kr/1230000/BidPublicInfoService04/getBidPblancListInfoThngPPSSrch"
+        "http://apis.data.go.kr/1230000/OrderPlanInfoService02/getOrderPlanListInfoPPSSrch",  # 발주계획 API
+        "http://apis.data.go.kr/1230000/BidPublicInfoService04/getBidPblancListInfoServcPPSSrch",  # 입찰공고(용역)
+        "http://apis.data.go.kr/1230000/BidPublicInfoService04/getBidPblancListInfoThngPPSSrch"  # 입찰공고(물품)
     ]
 
     for url in urls:
         params = {
             'serviceKey': API_KEY,
-            'numOfRows': '999',  # 6개월치 데이터이므로 누락 없게 최대치로 조회
+            'numOfRows': '999',
             'pageNo': '1',
-            'inqryDiv': '1',  # 공고게시일시 기준 조회
+            'inqryDiv': '1',
             'inqryBgnDt': six_months_ago + '0000',
             'inqryEndDt': today_str + '2359',
             'type': 'json'
@@ -52,26 +51,34 @@ def get_jodal_pblanc():
                 items = data.get('response', {}).get('body', {}).get('items', [])
                 if isinstance(items, list):
                     for item in items:
-                        title = item.get('bidNtceNm', '')
+                        # 발주계획과 입찰공고의 데이터 이름(태그명) 차이 해결
+                        title = item.get('bidNtceNm') or item.get('bsnsNm', '')  # 사업명/공고명
+                        inst = item.get('demandInsttNm', '알수없음')  # 수요기관
+                        url_link = item.get('bidNtceDtlUrl') or item.get('orderPlanDtlUrl', '#')  # 상세링크
 
-                        # 키워드가 포함되어 있는지 검사
+                        # 등록일시 추출 (발주계획은 'registDt', 입찰공고는 'bidNtceBgngDt')
+                        bgng_dt_str = item.get('bidNtceBgngDt') or item.get('registDt', '')
+
+                        # 구분표시 (발주계획인지 입찰공고인지 제목에 붙여주기)
+                        type_tag = "[발주계획]" if "OrderPlan" in url else "[입찰공고]"
+                        full_title = f"{type_tag} {title}"
+
+                        # 키wl드가 포함되어 있는지 검사
                         if any(kw in title for kw in KEYWORDS):
-                            bgng_dt_str = item.get('bidNtceBgngDt', '')
-
                             pblanc_data = {
-                                'title': title,
-                                'url': item.get('bidNtceDtlUrl', '#'),
-                                'inst': item.get('demandInsttNm', '알수없음'),
+                                'title': full_title,
+                                'url': url_link,
+                                'inst': inst,
                                 'date': bgng_dt_str
                             }
 
-                            # 🌟 날짜 분류 (오늘 날짜가 게시일시에 포함되어 있다면 '최신 업데이트' 처리)
+                            # 오늘 날짜 분기 처리
                             if today_str in bgng_dt_str.replace('-', ''):
                                 new_items.append(pblanc_data)
                             else:
                                 ongoing_items.append(pblanc_data)
         except Exception as e:
-            print(f"공고 조회 중 에러 발생: {e}")
+            print(f"조회 중 에러 발생: {e}")
 
     return new_items, ongoing_items
 
@@ -85,18 +92,18 @@ def make_messenger_text(new_items, ongoing_items):
         for idx, item in enumerate(new_items, 1):
             msg += f"{idx}. ✨ *[최신 업데이트] {item['title']}*\n"
             msg += f"   - 수요기관: {item['inst']}\n"
-            msg += f"   - 게시일시: {item['date']}\n"
+            msg += f"   - 등록일시: {item['date']}\n"
             msg += f"   - [공고 상세 링크 클릭]({item['url']})\n\n"
     else:
         msg += "오늘 새로 등록된 최신 공고는 없습니다.\n\n"
 
     msg += "--------------------------------------------------\n"
-    msg += "📅 *최근 6개월간 진행 중인 공고 리스트*\n\n"
+    msg += "📅 *최근 6개월간 진행 중인 공고/발주 리스트*\n\n"
 
     if ongoing_items:
         for idx, item in enumerate(ongoing_items, 1):
             msg += f"{idx}. *{item['title']}*\n"
-            msg += f"   - 수요기관: {item['inst']} | 게시일: {item['date'][:10]}\n"
+            msg += f"   - 수요기관: {item['inst']} | 등록일: {item['date'][:10]}\n"
             msg += f"   - [공고 상세 링크 클릭]({item['url']})\n\n"
     else:
         msg += "최근 6개월간 진행 중인 공고가 없습니다.\n"
@@ -125,10 +132,7 @@ def send_all(new_items, ongoing_items):
         try:
             slack_url = "https://slack.com/api/chat.postMessage"
             headers = {"Authorization": f"Bearer {SLACK_TOKEN}"}
-            payload = {
-                "channel": SLACK_CHANNEL,
-                "text": messenger_text
-            }
+            payload = {"channel": SLACK_CHANNEL, "text": messenger_text}
             res = requests.post(slack_url, headers=headers, json=payload, timeout=10)
             if res.json().get('ok'):
                 print("슬랙 알림 전송 완료")
@@ -152,13 +156,13 @@ def send_all(new_items, ongoing_items):
             for item in new_items:
                 html += f"<div style='background-color: #ffe6e6; padding: 10px; margin-bottom: 10px; border-left: 5px solid red;'>"
                 html += f"<b>[최신 업데이트] {item['title']}</b><br>"
-                html += f"수요기관: {item['inst']} | 게시일시: {item['date']}<br>"
+                html += f"수요기관: {item['inst']} | 등록일시: {item['date']}<br>"
                 html += f"<a href='{item['url']}'>👉 공고 바로가기</a></div>"
         else:
             html += "<p style='color: gray;'>오늘 등록된 최신 공고가 없습니다.</p>"
 
-        html += "<br><h3>📅 최근 6개월간 진행 중인 공고</h3><table border='1' cellpadding='5' style='border-collapse:collapse; width:100%;'>"
-        html += "<tr style='background-color:#f2f2f2;'><th>공고명</th><th>수요기관</th><th>게시일</th></tr>"
+        html += "<br><h3>📅 최근 6개월간 진행 중인 공고/발주계획</h3><table border='1' cellpadding='5' style='border-collapse:collapse; width:100%;'>"
+        html += "<tr style='background-color:#f2f2f2;'><th>구분 및 사업명</th><th>수요기관</th><th>등록일</th></tr>"
 
         if ongoing_items:
             for item in ongoing_items:
@@ -180,6 +184,6 @@ def send_all(new_items, ongoing_items):
 
 
 if __name__ == "__main__":
-    print("나라장터 6개월치 알림 로봇 구동 중...")
+    print("나라장터 발주계획 통합 로봇 구동 중...")
     new_list, ongoing_list = get_jodal_pblanc()
     send_all(new_list, ongoing_list)
